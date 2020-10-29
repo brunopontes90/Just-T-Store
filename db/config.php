71fea756d916f9a6a422743e7fa6cbccf688c49a
Bruno Pontes <53492661+brunopontes90@users.noreply.github.com>
2020-10-29 13:40:42 -0300

    criado objeto em config.php

--- db/config.php
+++ db/config.php
@@ -3,6 +3,12 @@ ini_set('display_errors', 1); //mostra os erros, em produção coloque 0
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$dsn = 'sqlsrv:Server=localhost\\SQLEXPRESS;Database=CADASTRO'; //localhost na maioria dos PCs dos alunos
-$user = 'sa';
-$password = '9012@TIBruno';
+class banco{
+    // $dsn = 'sqlsrv:Server=localhost\\SQLEXPRESS;Database=CADASTRO'; //localhost na maioria dos PCs dos alunos
+    public $cadastro = 'sqlsrv:Server=localhost\\SQLEXPRESS;Database=CADASTRO';
+    public $login = 'sqlsrv:Server=localhost\\SQLEXPRESS;Database=LOGIN';
+    public $user = 'sa';
+    public $password = '9012@TIBruno';
+}
+
+$obj = new banco();
